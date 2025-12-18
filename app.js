4c49cde415fb6b2c78aff2d893d8feb4a3d966de
rbvc8nfyv5-prog <rbvc8nfyv5@privaterelay.appleid.com>
2025-12-18 20:49:57 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+(function () {
+
+  document.body.innerHTML = `
+    <div style="color:#fff;padding:20px;text-align:center">
+      <h2>Projeto Independente</h2>
+      <p>Base criada com sucesso.</p>
+    </div>
+  `;
+
+})();
